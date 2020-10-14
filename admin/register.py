2c75b4d75fc37acb0c828c028e762690465adfd6
= <awesomepranshu@gmail.com>
2020-10-14 12:52:08 +0530

    Add register script

--- admin/register.py
+++ admin/register.py
@@ -7,7 +7,6 @@ responses = pd.read_csv('responses.csv')
 cred = credentials.Certificate("creds.json")
 firebase_admin.initialize_app(cred)
 
-
 db = firestore.client()
 
 codes = {}
@@ -19,7 +18,7 @@ for ind in responses.index:
         'members': [responses['Team Leader'][ind], responses['Team Member 2'][ind]],
         'answers': {'1': ''},
     }
-    db.collection('test').document(teamCode).set(data, merge=True)
-    db.collection('scorecardTest').document(teamCode).set({'1': 0}, merge=True)
+    db.collection('registered').document(teamCode).set(data, merge=True)
+    db.collection('scorecard').document(teamCode).set({'1': 0}, merge=True)
     codes[str(ind)] = teamCode
-db.collection('teamCodeTest').document('codes').set(codes, merge=True)
+db.collection('teamCode').document('codes').set(codes, merge=True)
